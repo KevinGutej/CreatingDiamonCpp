3ad071ad9a9ca931203f21677633de2917d767db
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2022-12-14 21:43:07 +0100

    Add files via upload

--- diamond.cpp
+++ diamond.cpp
@@ -6,14 +6,35 @@ int main() {
 	int i,j,row,space;
 	cout << "Enter number of rows: ";
 	cin >> row;
-	space = rows-1;
-	for(i=1; i<=rows;i++)
+	space = row-1;
+	for(i=1; i<=row; i++)
 	{
-		for(j=1;j<=space;j++)
+		for(j=1;j<=space; j++)
 		{
-			
+			cout << " ";
+			space--;
+			for(j=1; j<=(2*i-1); j++)
+			{
+				cout << "*";
+				cout << endl;
+			}
 		}
 	}
+	space = 1;
+	for(i=1;i<=(row-1); i++)
+	{
+		for(j=1;j<=space; j++)
+		{
+			cout << " ";
+			space++;
+			for(j=1;j<=(2*(row-i)-1); j++)
+			{
+				cout << "*";
+				cout << endl;
+			}
+		}
+	}
+	cout << endl;
 	return 0;
 }
 
