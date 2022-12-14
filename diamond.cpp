26ed923a1419a7fc4200c0c17df2d25b7fc34cdd
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2022-12-14 21:38:25 +0100

    Add files via upload

---
+++ diamond.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int i,j,row,space;
+	cout << "Enter number of rows: ";
+	cin >> row;
+	space = rows-1;
+	for(i=1; i<=rows;i++)
+	{
+		for(j=1;j<=space;j++)
+		{
+			
+		}
+	}
+	return 0;
+}
+
